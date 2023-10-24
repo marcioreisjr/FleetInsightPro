e871ec37aa5ca1e5ce487ec8c141fa5c8dac2c91
Marcio Reis Jr <marcioreisjr@gmail.com>
2023-10-23 17:18:58 -0700

    Completed Manufacturer List

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
 import ManufacturerForm from './inventory/ManufacturerForm';
+import ManufacturerList from './inventory/ManufacturerList';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/manufacturers/create" element={<ManufacturerForm />} />
+          <Route path="/manufacturers" element={<ManufacturerList />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/app/src/inventory/ManufacturerList.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react";
+
+
+function ManufacturerList() {
+    /*
+    * We will store the list of manufacturers in the manufacturers state
+    * To ger the list of manufacturers, we need to send a GET request to the
+    * server at http://localhost:8100/api/manufacturers/ and will receive a
+    * JSON response that looks like this:
+    * {
+    *   "manufacturers": [
+    *    {
+    *       "href": "/api/manufacturers/1/",
+    *       "id": 1,
+    *       "name": "Daimler-Chrysler"
+    *    },
+    *   ]
+    * }
+    */
+    const [manufacturers, setManufacturers] = React.useState([]);
+
+    function getManufacturers() {
+        const url = "http://localhost:8100/api/manufacturers/";
+        fetch(url)
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json();
+                }
+            })
+            .then((response) => {
+                setManufacturers(response.manufacturers);
+            })
+    }
+
+    useEffect(() => {
+        getManufacturers();
+    }, []);
+
+    return (
+        <div>
+            <h1 className="text-center my-4">Manufacturer List</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {manufacturers.map((manufacturer) => (
+                        <tr key={manufacturer.id}>
+                            <td>{manufacturer.name}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default ManufacturerList;
