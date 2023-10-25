12f555f5f65beb2a8537dbb75eba39841b476df5
Howard Chung <howardchung1004@gmail.com>
2023-10-25 16:23:49 -0400

    sales poller updated

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -21,7 +21,6 @@ def poll(repeat=True):
             # Do not copy entire file
             response = requests.get("http://project-beta-inventory-api-1:8000/api/automobiles/")
             content = json.loads(response.content)
-            print(f"######-CONTENT-##### {content}")
             for auto in content["autos"]:
                 AutomobileVO.objects.update_or_create(
                     import_href=auto["href"],
