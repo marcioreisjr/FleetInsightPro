71c821e3b0cefa9a455090ce7630628dc0b2e696
Marcio Reis Jr <marcioreisjr@gmail.com>
2023-10-27 17:36:11 -0700

    Enhancements to the client interface - Solved webpack warnings - Formated the phone number

--- ghi/app/src/sales/CustomerForm.jsx
+++ ghi/app/src/sales/CustomerForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function CustomerForm({ setAlert }) {
     /*
@@ -12,30 +12,50 @@ function CustomerForm({ setAlert }) {
         "phone_number": "111-233-1267"
     }
     */
-    const [first_name, setFirstName] = React.useState("");
+    const [first_name, setFirstName] = useState("");
     function handleFirstNameChange(event) {
         setFirstName(event.target.value);
     }
 
-    const [last_name, setLastName] = React.useState("");
+    const [last_name, setLastName] = useState("");
     function handleLastNameChange(event) {
         setLastName(event.target.value);
     }
 
-    const [address, setAddress] = React.useState("");
+    const [address, setAddress] = useState("");
     function handleAddressChange(event) {
         setAddress(event.target.value);
     }
 
-    const [phone_number, setPhoneNumber] = React.useState("");
+    const [phone_number, setPhoneNumber] = useState("");
     function handlePhoneNumberChange(event) {
         setPhoneNumber(event.target.value);
     }
 
-   function handleSubmit(event) {
+    function fixPhoneNumber(phone_number) {
+        phone_number = phone_number.replace(/[^+\d]/g, "");
+        let re = (/(\+\d{1,3})?\(?(\d{3})\)?(\d{3})(\d{4})/g)
+            .exec(phone_number);
+        if (re) {
+            return `${re[1] ? re[1].trim() :
+                ""} (${re[2]})${re[3]}-${re[4]}`.trim();
+        }
+        return phone_number.trim();
+    }
+
+    function handlePhoneBlur(event) {
+        setPhoneNumber(fixPhoneNumber(event.target.value));
+    }
+
+    function handleSubmit(event) {
         event.preventDefault();
         const url = "http://localhost:8090/api/customers/";
-        const data = { first_name, last_name, address, phone_number };
+        const data = {
+            first_name,
+            last_name,
+            address,
+            phone_number: fixPhoneNumber(phone_number),
+        };
         fetch(url, {
             method: "POST",
             headers: {
@@ -66,15 +86,16 @@ function CustomerForm({ setAlert }) {
                             <label>First Name...</label>
                         </div>
                         <div className="form-floating mb-3">
-                            <input value={last_name} onChange={handleLastNameChange} required type="type" name="last_name" id="last_name" placeholder="Last Name" className="form-control"/>
+                            <input value={last_name} onChange={handleLastNameChange} required type="type" name="last_name" id="last_name" placeholder="Last Name" className="form-control" />
                             <label>Last Name...</label>
                         </div>
                         <div className="form-floating mb-3">
-                            <input value={address} onChange={handleAddressChange} required type="text" name="address" id="address" placeholder="Address" className="form-control"/>
+                            <input value={address} onChange={handleAddressChange} required type="text" name="address" id="address" placeholder="Address" className="form-control" />
                             <label>Address...</label>
                         </div>
                         <div className="form-floating mb-3">
-                            <input value={phone_number} onChange={handlePhoneNumberChange} required type="text" name="address" id="address" placeholder="Phone Number" className="form-control"/>
+                            <input value={phone_number} onChange={handlePhoneNumberChange} onBlur={handlePhoneBlur}
+                            required type="tel" name="address" id="address" placeholder="Phone Number" className="form-control" />
                             <label>Phone Number...</label>
                         </div>
                         <button className="btn btn-secondary">
